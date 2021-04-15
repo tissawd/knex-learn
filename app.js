20d383fa192498eb513173886efbec7a3f26d5c4
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-04-15 11:15:14 -0500

    added express server to read from  db

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+
+app.use(bodyParser.json);
+
+app.get('/movies', (req, res) => {
+  knex
+  .select('*')
+  .from('movies')
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(404).json({message: 'The data you are looking for could not be found. Please try again'}));
+})
+
+
+app.listen(PORT, () => {
+  console.log(`The Express server is running on port ${port}`);
+});
